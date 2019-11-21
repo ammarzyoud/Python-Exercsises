4df36bd4721d4e07b5a33c40b17107dc2e5380b0
ammarzyoud <ammar.m.alzyoud@gmail.com>
2019-11-21 14:38:30 +0200

    Third-Day-EX

---
+++ Exercises-day-3.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 13:49:08 2019
+
+@author: Ammar
+"""
+
+# Exercises
+
+# Exercise 1
+#myList = [1,20,100,5,3]
+#for i in myList:
+#    print(i)
+
+# Exercise 2
+#myList = [1,20,100,5,3]
+#print(sum(myList)) output ----> 129
+
+# Exercise 3
+#myList = [1,20,100,5,3]
+#print(max(myList)) output -----> 100
+
+# Exercise 4
+#a = [10,20,30,20,10,50,60,40,80,50,40]
+#l = list(set(a))
+#print (l) output ----> [40, 10, 80, 50, 20, 60, 30]
+
+# Exercise 5
+#a = [10,20,30,20,10,50,60,40,80,50,40]
+#if len(a) == 0:
+#    print ("List is Empty",len(a))
+#else:
+#    print ("List Length is ",len(a))
+
+# Exercise 6
+#myTuple = ( 1,20,"Ayham",5,3,[2,5,8] )
+#for i in myTuple:
+#    print(i)
+
+# Exercise 7
+#num_set = set([0, 1, 2, 3, 4, 5])
+#for i in num_set:
+#    print(i)
+    
+    
+# Exercise 8
+#set1 = {0, 1, 2, 3, 4, 5}
+#set2 = {3, 4, 6, 7}
+#set3 = set1.intersection(set2) 
+#print(set3)
+
+# Exercise 9
+#setx = set(["green","blue"])
+#sety = set(["blue","yellow"])
+#seta = setx | sety
+#print (seta) output -----> {'blue', 'yellow', 'green'}
+
+# Exercise 10
+#seta = set([5,10,3,15,2,20])
+#print (len(seta)) output ----> 6
+
+# Exercise 11
+#dic1 = {1:10,2:20}
+#dic2 = {3:30,4:40}
+#dic3 = {5:50,6:60}
+#dic4 = {}
+#for d in (dic1,dic2,dic3):
+#    dic4.update(d)
+#print (dic4) output -----> {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
+# Exercise 12
+#a = "Hello, World"
+#print (a[1])
+#print (a[2:5])
+#print (a[-5:-2])
+#print (len(a))
+#print (a.lower())
+#print (a.replace("H","J"))
+#output ------>
+#e
+#llo
+#Wor
+#12
+#hello, world
+#Jello, World
+
+
+
+
+
+
+
